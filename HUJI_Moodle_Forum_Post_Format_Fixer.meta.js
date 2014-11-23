60eef846c147bb1fbff0eca5e2f70b1e8f5f1aa1
shlomisha <shlomi.shasha@gmail.com>
2014-11-24 01:20:09 +0200

    Added files

---
+++ HUJI_Moodle_Forum_Post_Format_Fixer.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @id             HUJI_Moodle_Forum_Post_Format_Fixer
+// @name           HUJI Moodle Forum Post Format Fixer
+// @namespace      shlomisha
+// @author         shlomisha
+// @version        2.1.1
+// @description	   Adds "change direction" link to the forum posts in the moodle of the Hebrew University of Jerusalem, Israel (HUJI), so it is easier to read posts combined Hebrew and English in the same paragraph
+// @include        http://moodle.cs.huji.ac.il/cs*/mod/forum/discuss.php?*
+// @include        http://moodle.cs.huji.ac.il/cs*/mod/forumng/discuss.php?*
+// @include        http://moodle.cs.huji.ac.il/cs*/mod/forumng/view.php?*
+// @include        http://moodle.huji.ac.il/hu*/mod/forum/discuss.php?*
+// @run-at         document-end
+// ==/UserScript==
